788d890e97c6c6ab13ff9fd0275f2cfd5c4dc397
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-02-28 11:09:06 +0700

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,16 +1,12 @@
 #include "Enemy.h"
 
-Enemy::Enemy()
-{
-	x = rand() % SCREEN_WIDTH;
-	y = rand() % SCREEN_HEIGHT;
-	angle = rand() % 360;
-	type = 1;
-	points = { {0, 10}, {0, 20}, {10, 30}, {10, 20}, {20, 30}, {30, 20} };
-}
+Enemy::Enemy() : x{ rand() % SCREEN_WIDTH }, y{ rand() % SCREEN_HEIGHT }, 
+				 angle{ rand() % 360 }, type{1}, points{ {0, 10}, {0, 20}, {10, 30}, {10, 20}, {20, 30}, {30, 20} } 
+{}
+
 void Enemy::Draw()
 {
-	for (int i = 1; i < points.size(); i++)
+	for (int i = 1; i < points.size(); ++i)
 	{
 		DrawLine(points[i - 1].x + x, points[i - 1].y + y, points[i].x + x, points[i].y + y, 255, 255, 0);
 	}
@@ -23,8 +19,4 @@ void Enemy::Update()
 	if (x < 0) x += SCREEN_WIDTH;
 	if (y > SCREEN_HEIGHT) y -= SCREEN_HEIGHT;
 	if (y < 0) y += SCREEN_HEIGHT;
-}
-Enemy::~Enemy()
-{
-
 }
